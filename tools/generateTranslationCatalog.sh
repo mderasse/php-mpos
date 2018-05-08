3c280e5d184c915f7d6ca7d19e99be33974bb93a
Matthieu 'Atomics' Derasse <github@derasse.fr>
2018-05-08 21:52:20 +0200

    Translation Tools

---
+++ tools/generateTranslationCatalog.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+./tsmarty2c.php -o mpos.pot ../templates/bootstrap/
+
+echo "" >> mpos.pot
+echo "#: ../templates/bootstrap//global/header.tpl:70" >> mpos.pot
+echo "msgid \"en\"" >> mpos.pot
+echo "msgstr \"\"" >> mpos.pot
+
+echo "" >> mpos.pot
+echo "#: ../templates/bootstrap//global/header.tpl:70" >> mpos.pot
+echo "msgid \"fr\"" >> mpos.pot
+echo "msgstr \"\"" >> mpos.pot
+
+echo "" >> mpos.pot
+echo "#: ../templates/bootstrap//global/header.tpl:70" >> mpos.pot
+echo "msgid \"es\"" >> mpos.pot
+echo "msgstr \"\"" >> mpos.pot
+
+echo "" >> mpos.pot
+echo "#: ../templates/bootstrap//global/header.tpl:70" >> mpos.pot
+echo "msgid \"de\"" >> mpos.pot
+echo "msgstr \"\"" >> mpos.pot
+
+echo "" >> mpos.pot
+echo "#: ../templates/bootstrap//global/header.tpl:70" >> mpos.pot
+echo "msgid \"it\"" >> mpos.pot
+echo "msgstr \"\"" >> mpos.pot
+
+echo "" >> mpos.pot
+echo "#: ../templates/bootstrap//global/header.tpl:70" >> mpos.pot
+echo "msgid \"ru\"" >> mpos.pot
+echo "msgstr \"\"" >> mpos.pot
+
+echo "" >> mpos.pot
+echo "#: ../templates/bootstrap//global/header.tpl:70" >> mpos.pot
+echo "msgid \"zh\"" >> mpos.pot
+echo "msgstr \"\"" >> mpos.pot
